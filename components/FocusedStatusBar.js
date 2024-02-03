fc14087bb1d786ff22f066bf5c7f1605d961348f
Ganesh Shinde <sindheganesh2004@gmail.com>
2024-02-03 21:52:43 +0530

    CReated the basic of app

---
+++ components/FocusedStatusBar.js
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import { useIsFocused } from '@react-navigation/core'
+import { StatusBar } from "react-native"
+
+const FocusedStatusBar = (props) => {
+  const isFocused = useIsFocused();
+
+  return (
+    isFocused ? <StatusBar animated={true} {...props} /> : null
+
+    
+  )
+}
+
+export default FocusedStatusBar
---
+++ components/NFTCard.js
@@ -0,0 +1,36 @@
+import { View, Image, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+import { COLORS, SHADOWS, SIZES, assets } from "../constants";
+
+const NFTCard = ({ data }) => {
+  const navigation = useNavigation();
+
+  return (
+    <View
+      style={{
+        backgroundColor: COLORS.white,
+        borderRadius: SIZES.font,
+        marginBottom: SIZES.extraLarge,
+        margin: SIZES.base,
+        ...SHADOWS.dark,
+      }}
+    >
+      <View style={{ width: "100%", height: 250 }}>
+        <Image
+          source={data.Image}
+          resizeMode="cover"
+          style={{
+            width: "100%",
+            height: "100%",
+            borderTopLeftRadius: SIZES.font,
+            borderTopRightRadius: SIZES.font,
+          }}
+        />
+      </View>
+      <Text>NFTCard</Text>
+    </View>
+  );
+};
+
+export default NFTCard;
