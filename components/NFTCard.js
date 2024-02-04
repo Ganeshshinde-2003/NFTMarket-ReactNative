80495b8f464b8e645c78de5bdc29fc2e64274883
Ganesh Shinde <sindheganesh2004@gmail.com>
2024-02-04 10:43:51 +0530

    Created the flat view

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -2,6 +2,9 @@ import { View, Image, Text } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 import { COLORS, SHADOWS, SIZES, assets } from "../constants";
+import { CircleButton, RectButton } from "./Button";
+
+import { SubInfo, EthPrice, NFTTile } from "./SUbInfo";
 
 const NFTCard = ({ data }) => {
   const navigation = useNavigation();
@@ -18,7 +21,7 @@ const NFTCard = ({ data }) => {
     >
       <View style={{ width: "100%", height: 250 }}>
         <Image
-          source={data.Image}
+          source={data.image}
           resizeMode="cover"
           style={{
             width: "100%",
@@ -27,8 +30,34 @@ const NFTCard = ({ data }) => {
             borderTopRightRadius: SIZES.font,
           }}
         />
+        <CircleButton imgUrl={assets.heart} right={10} top={10} />
       </View>
-      <Text>NFTCard</Text>
+
+      <SubInfo />
+
+      <View style={{ width: "100%", padding: SIZES.font }}>
+        <NFTTile
+          title={data.name}
+          subTitle={data.creator}
+          titleSize={SIZES.large}
+          subTitleSize={SIZES.small}
+        />
+        <View
+          style={{
+            marginTop: SIZES.font,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <EthPrice price={data.price} />
+          <RectButton
+            minWidth={120}
+            fontSize={SIZES.font}
+            handlePress={() => navigation.navigate("Details", { data })}
+          />
+        </View>
+      </View>
     </View>
   );
 };
--- components/index.js
+++ components/index.js
@@ -1,5 +1,6 @@
 import HomeHeader from "./HomeHeader";
 import FocusedStatusBar from "./FocusedStatusBar";
 import NFTCard from "./NFTCard";
+import { RectButton, CircleButton } from "./Button";
 
-export { HomeHeader, FocusedStatusBar, NFTCard };
+export { HomeHeader, FocusedStatusBar, NFTCard, RectButton, CircleButton };
